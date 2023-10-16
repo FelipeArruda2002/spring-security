66617f5f5d416bef78da4afc202435e2e2ec403d
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2023-10-16 20:57:13 -0300

    Projeto/rotas sem segurança

--- src/main/java/br/com/felipearruda/springsecurity/SpringSecurityApplication.java
+++ src/main/java/br/com/felipearruda/springsecurity/SpringSecurityApplication.java
@@ -2,12 +2,28 @@ package br.com.felipearruda.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+	@RestController
+	class HttpController {
+
+		@GetMapping("/publica")
+		public String rotaPublica() {
+			return "Rota pública, fique à vontade para bloquear!";
+		}
+
+		@GetMapping("/privada")
+		public String rotaPrivada() {
+			return "Rota privada, apenas pessoal autorizado";
+		}
 	}
 
 }
