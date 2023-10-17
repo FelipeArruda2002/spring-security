ebf31957d7c3278a1fd97fad77edcff4ceafb0f6
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2023-10-16 21:37:32 -0300

    Autenticação no Google com Oauth2

--- src/main/java/br/com/felipearruda/springsecurity/SpringSecurityApplication.java
+++ src/main/java/br/com/felipearruda/springsecurity/SpringSecurityApplication.java
@@ -2,8 +2,12 @@ package br.com.felipearruda.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.StringReader;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -21,8 +25,15 @@ public class SpringSecurityApplication {
 		}
 
 		@GetMapping("/privada")
-		public String rotaPrivada() {
-			return "Rota privada, apenas pessoal autorizado";
+		public String rotaPrivada(@AuthenticationPrincipal OidcUser principal) {
+			return String.format("""
+					<h1>Oauth2 🔐  </h1>
+				<h3>Principal: %s</h3>
+				<h3>Email: %s</h3>
+				<h3>Authorities: %s</h3>
+				<h3>JWT: %s</h3>
+				""", principal, principal.getAttribute("email"), principal.getAuthorities(),
+					principal.getIdToken().getTokenValue());
 		}
 	}
 
