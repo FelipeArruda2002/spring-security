7c4765fb32b1770ead0879253c0ce3a7a05cf581
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2023-10-16 22:03:05 -0300

    Autenticação com JWT

--- src/main/java/br/com/felipearruda/springsecurity/SpringSecurityApplication.java
+++ src/main/java/br/com/felipearruda/springsecurity/SpringSecurityApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.StringReader;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -25,7 +24,7 @@ public class SpringSecurityApplication {
 		}
 
 		@GetMapping("/privada")
-		public String rotaPrivada(@AuthenticationPrincipal OidcUser principal) {
+		public String cookie(@AuthenticationPrincipal OidcUser principal) {
 			return String.format("""
 					<h1>Oauth2 🔐  </h1>
 				<h3>Principal: %s</h3>
@@ -34,6 +33,15 @@ public class SpringSecurityApplication {
 				<h3>JWT: %s</h3>
 				""", principal, principal.getAttribute("email"), principal.getAuthorities(),
 					principal.getIdToken().getTokenValue());
+		}
+
+		@GetMapping("/jwt")
+		public String jwt(@AuthenticationPrincipal Jwt jwt) {
+			return String.format("""
+				Principal: %s\n
+				Email: %s\n
+				JWT: %s\n
+				""", jwt.getClaims(), jwt.getClaim("email"), jwt.getTokenValue());
 		}
 	}
 
